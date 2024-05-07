38d1ea34dc45a52aec5cc7c4dda9480e0f4a0b9e
Elvis Githinji <109733200+kisinja@users.noreply.github.com>
2024-05-07 18:08:36 +0300

    Changes to be committed: 	modified:   index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const morgan = require('morgan');
 const cors = require('cors');
 const app = express();
 
+// IMPORTS
 const { connectdb } = require('./db');
 const { verifyToken, verifyTokenAndAdmin } = require("./middleware/auth");
 const authRouter = require('./routes/auth');
@@ -11,22 +12,27 @@ const productRouter = require('./routes/product');
 const cartRouter = require('./routes/cart');
 const orderRouter = require('./routes/order');
 
+// PORT NUMBER
 const port = 8050;
 
+// MIDDLEWARES
 app.use(morgan('dev'));
 app.use(cors());
-
 app.use(express.json());
+
+// BASE IMPORTS
 app.use('/auth', authRouter);
 app.use('/users', verifyToken, userRouter);
 app.use('/products', verifyTokenAndAdmin, productRouter);
 app.use('/carts', verifyToken, cartRouter);
 app.use('/orders', verifyToken, orderRouter);
 
+// API TESTING
 app.get("/", (req, res) => {
     res.send("API inafanya kazi sasa enda uimalizie kazi yako").status(200);
 });
 
+// SERVER START
 const start_server = () => {
     connectdb();
     app.listen(port, () => {
